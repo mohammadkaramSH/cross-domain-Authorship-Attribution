872f15c183ea1c995e760f12fd64121f17c62d80
mohammadkaramSH <49808909+mohammadkaramSH@users.noreply.github.com>
2019-04-20 14:36:31 +0430

    Create Pan_Competitions.py

---
+++ Pan_Competitions.py
@@ -0,0 +1,47 @@
+class Pan:
+    #preperacessing
+    def Tokenizer(self , text, **args):
+        pass
+    def Pos_tagger(self, text, **args):
+        pass
+    def Terms_counter(self, text, **args):
+        pass
+
+    #visualization
+    def Pie(self, **args):
+        pass
+    def Bar(self, **args):
+        pass
+    def Scatter(self, **args):
+        pass
+    def Hist(self, **args):
+        pass
+
+    #machine learning
+    #performance
+    def Precision(self, perdiction, truth):
+        pass
+    def Recall(self, perdiction, truth):
+        pass
+    def F1(self, perdicition, truth):
+        pass
+    def Accuracy(self, perdicition , truth):
+        pass
+    def C_at_1(self, perdicition, truth):
+        pass
+
+class Cross_Domain_Authorship_Attribution(Pan):
+    pass
+    
+class Style_Change_Detection(Pan):
+    pass
+
+class Celebrity_Profiling(Pan):
+    pass
+
+class Bots_And_Gender_Profiling(Pan):
+    pass
+
+def main():
+    pass
+if __name__ == "__main__": main()
